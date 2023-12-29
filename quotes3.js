b7a7279b7db1c1bde8dd5ee0b1cf833b35641776
Josh Langfield <43525627+jlangfield@users.noreply.github.com>
2023-12-29 12:13:33 -0600

    Add files via upload

--- quotes3.js
+++ quotes3.js
@@ -1,56 +1,250 @@
 const localQuotes3 = [
   {
     text: "Do not be in a hurry to succeed. What would you have to live for afterwards?",
-    author: "Interpretation: Emphasizes the value of patience, humility, and the importance of the journey over the destination.",
+    author: "Interpretation: Interpretation: Emphasizes the value of patience, humility, and the importance of the journey over the destination. #HopiNation",
   },
   {
     text: "Do not be in a hurry to succeed. What would you have to live for afterwards?",
-    author: "Interpretation: Emphasizes the value of patience, humility, and the importance of the journey over the destination."
+    author: "Interpretation: Interpretation: Emphasizes the value of patience, humility, and the importance of the journey over the destination. #HopiNation",
   },
   {
     text: "All dreams spin out from the same web.",
-    author: "Interpretation: Conveys the interconnectedness of all life and experiences."
+    author: "Interpretation: Interpretation: Conveys the interconnectedness of all life and experiences. #HopiNation",
   },
   {
     text: "To watch us dance is to hear our hearts speak.",
-    author: "Interpretation: Suggests that through traditional dances, the Hopi express their cultural identity and spirituality."
+    author: "Interpretation: Interpretation: Suggests that through traditional dances, the Hopi express their cultural identity and spirituality. #HopiNation",
   },
   {
     text: "The one who tells the stories rules the world.",
-    author: "Interpretation: Highlights the power of storytelling in preserving and transmitting cultural knowledge and values."
+    author: "Interpretation: Interpretation: Highlights the power of storytelling in preserving and transmitting cultural knowledge and values. #HopiNation",
   },
   {
     text: "Don't be afraid to cry. It will free your mind of sorrowful thoughts.",
-    author: "Interpretation: Encourages emotional expression as a means of healing and releasing burdens."
+    author: "Interpretation: Interpretation: Encourages emotional expression as a means of healing and releasing burdens. #HopiNation",
   },
   {
     text: "We will be known forever by the tracks we leave.",
-    author: "Interpretation: Encourages mindful actions, as they contribute to one's legacy and the collective history of the community."
+    author: "Interpretation: Interpretation: Encourages mindful actions, as they contribute to one's legacy and the collective history of the community. #HopiNation",
   },
   {
     text: "Knowledge is a gift from our Creator; it was given to us to pass on to others.",
-    author: "Interpretation: Stresses the importance of sharing wisdom and knowledge for the benefit of the community."
+    author: "Interpretation: Interpretation: Stresses the importance of sharing wisdom and knowledge for the benefit of the community. #HopiNation",
   },
   {
     text: "When you arise in the morning, give thanks for the morning light, for your life, and strength. Give thanks for your food and the joy of living. If you see no reason for giving thanks, the fault lies in yourself.",
-    author: "Interpretation: Encourages gratitude and mindfulness in appreciating the blessings of each day."
+    author: "Interpretation: Interpretation: Encourages gratitude and mindfulness in appreciating the blessings of each day. #HopiNation",
   },
   {
     text: "A frog does not drink up the pond in which it lives.",
-    author: "Interpretation: Encourages responsible and sustainable use of resources, respecting the balance of nature."
+    author: "Interpretation: Interpretation: Encourages responsible and sustainable use of resources, respecting the balance of nature. #HopiNation",
   },
   {
     text: "It is not the answer that enlightens, but the question.",
-    author: "Interpretation: Emphasizes the value of curiosity and the journey of seeking knowledge."
+    author: "Interpretation: Interpretation: Emphasizes the value of curiosity and the journey of seeking knowledge. #HopiNation",
   },
   {
     text: "Those who are at peace in their hearts already are in the great shelter of life.",
-    author: "Interpretation: Suggests that inner peace is a source of strength and protection."
+    author: "Interpretation: Interpretation: Suggests that inner peace is a source of strength and protection. #HopiNation",
   },
   {
     text: "The heart of the Hopi way of life is not religion, but spirituality.",
-    author: "Interpretation: Highlights the distinction between organized religion and the spiritual essence of Hopi teachings."
+    author: "Interpretation: Interpretation: Highlights the distinction between organized religion and the spiritual essence of Hopi teachings. #HopiNation",
   },
-  
-  
+  {
+    text: "The path to the heart is through the mind.",
+    author: "Interpretation: Interpretation: Emphasizes the connection between intellectual understanding and emotional well-being. #HopiNation",
+  },
+  {
+    text: "A bird does not sing because it has an answer; it sings because it has a song.",
+    author: "Interpretation: Encourages self-expression and creativity without the need for a specific purpose. #HopiNation",
+  },
+  {
+    text: "Those who have one foot in the canoe and one foot in the boat are going to fall into the river.",
+    author: "Interpretation: Advises against trying to maintain conflicting or dual commitments, as it may lead to failure. #HopiNation",
+  },
+  {
+    text: "Before you judge a man, walk a mile in his shoes.",
+    author: "Interpretation: Encourages empathy and understanding by considering another person's perspective. #HopiNation",
+  },
+  {
+    text: "What you see with your eyes shut is what counts.",
+    author: "Interpretation: Stresses the importance of inner vision, intuition, and spiritual insight. #HopiNation",
+  },
+  {
+    text: "He who tells the truth is never wrong.",
+    author: "Interpretation: Emphasizes the value of honesty and integrity in all actions and communications. #HopiNation",
+  },
+  {
+    text: "The weakness of the enemy makes our strength.",
+    author: "Interpretation: Highlights the idea that challenges and adversaries can contribute to personal and collective strength. #HopiNation",
+  },
+  {
+    text: "The heart knows what the tongue can't say.",
+    author: "Interpretation: Expresses the idea that there are feelings and truths that go beyond verbal expression. #HopiNation",
+  },
+  {
+    text: "It takes a thousand voices to tell a single story.",
+    author: "Interpretation: Emphasizes the diversity of perspectives and the collective nature of storytelling. #HopiNation",
+  },
+  {
+    text: "Those who are kind benefit themselves; those who are cruel harm themselves.",
+    author: "Interpretation: Encourages kindness and emphasizes the reciprocal nature of one's actions. #HopiNation",
+  },
+  {
+    text: "When you plant a seed of love, it is you that blossoms.",
+    author: "Interpretation: Suggests that acts of love and kindness bring fulfillment and personal growth. #HopiNation",
+  },
+  {
+    text: "You can't wake a person who is pretending to be asleep.",
+    author: "Interpretation: Metaphorically points out the challenge of reaching someone who is willfully ignorant or avoiding reality. #HopiNation",
+  },
+  {
+    text: "Time is a created thing. To say 'I don't have time' is like saying, 'I don't want to.'",
+    author: "Interpretation: Encourages taking responsibility for how time is used and acknowledging personal priorities. #HopiNation",
+  },
+  {
+    text: "Do not be afraid to go a little out of your depth.",
+    author: "Interpretation: Encourages taking risks and stepping outside one's comfort zone for personal growth. #HopiNation",
+  },
+  {
+    text: "No tree has branches so foolish as to fight among themselves.",
+    author: "Interpretation: Uses the metaphor of a tree to emphasize the importance of unity and harmony within a community. #HopiNation",
+  },
+  {
+    text: "When the sun sets, no candle can replace it.",
+    author: "Interpretation: Expresses the irreplaceable nature of certain individuals or qualities. #HopiNation",
+  },
+  {
+    text: "Wash your spirit clean.",
+    author: "Interpretation: Encourages spiritual purification, suggesting the importance of maintaining a pure and positive mindset. #HopiNation",
+  },
+  {
+    text: "Happiness is not having what you want, but wanting what you have.",
+    author: "Interpretation: Encourages contentment and appreciation for the present moment and current blessings. #HopiNation",
+  },
+  {
+    text: "The longest journey is the journey inward.",
+    author: "Interpretation: Encourages self-reflection and the exploration of one's inner thoughts and feelings. #HopiNation",
+  },
+  {
+    text: "The frog does not drink up the pond in which it lives.",
+    author: "Interpretation: Encourages responsible and sustainable use of resources, respecting the balance of nature. #HopiNation",
+  },
+  {
+    text: "When you think you know, know you don't.",
+    author: "Interpretation: Encourages humility and the acknowledgment that there is always more to learn and understand. #HopiNation",
+  },
+  {
+    text: "Don't carry a grudge; while you're carrying a grudge, the other guy's out dancing.",
+    author: "Interpretation: Advises against holding onto resentment, as it only burdens oneself, while the perceived wrongdoer moves on. #HopiNation",
+  },
+  {
+    text: "A good chief gives, he does not take.",
+    author: "Interpretation: Highlights the qualities of leadership that prioritize giving, serving, and selflessness. #HopiNation",
+  },
+  {
+    text: "We will be known forever by the tracks we leave.",
+    author: "Interpretation: Encourages mindful actions, as they contribute to one's legacy and the collective history of the community. #HopiNation",
+  },
+  {
+    text: "Listen, or your tongue will make you deaf.",
+    author: "Interpretation: Emphasizes the importance of attentive listening and thoughtful speech. #HopiWisdom"
+  },
+  {
+    text: "Seek wisdom, not knowledge. Knowledge is of the past; wisdom is of the future.",
+    author: "Interpretation Distinguishes between factual knowledge and the deeper understanding that comes with wisdom. #HopiWisdom"
+  },
+  {
+    text: "The heart of a fool is in his mouth, but the mouth of a wise man is in his heart.",
+    author: "Interpretation Highlights the value of thoughtful and wise speech over impulsive and foolish words. #HopiWisdom"
+  },
+  {
+    text: "When you speak, speak with kindness. If you can't speak with kindness, don't speak at all.",
+    author: "Interpretation Encourages compassionate communication and the avoidance of harmful speech. #HopiWisdom"
+  },
+  {
+    text: "A mistake is a lesson, not a loss.",
+    author: "Interpretation Encourages a positive perspective on learning from mistakes and setbacks. #HopiWisdom"
+  },
+  {
+    text: "Patience is the key to paradise.",
+    author: "Interpretation Stresses the importance of patience in achieving positive outcomes and lasting fulfillment. #HopiWisdom"
+  },
+  {
+    text: "Don't let yesterday use up too much of today.",
+    author: "Interpretation Encourages living in the present moment and not dwelling excessively on the past. #HopiWisdom"
+  },
+  {
+    text: "The hardest arithmetic to master is that which enables us to count our blessings.",
+    author: "Interpretation Encourages gratitude and appreciation for the positive aspects of life. #HopiWisdom"
+  },
+  {
+    text: "Do not grieve over what has passed unless it makes you work for what is about to come.",
+    author: "Interpretation Encourages using past experiences as motivation for future growth and improvement. #HopiWisdom"
+  },
+  {
+    text: "Don't be afraid to start over. It's a chance to rebuild what you want.",
+    author: "Interpretation Emphasizes the opportunity for renewal and growth that comes with starting anew. #HopiWisdom"
+  },
+  {
+    text: "The best way to predict the future is to create it.",
+    author: "Interpretation Encourages taking proactive steps to shape one's own destiny and outcomes. #HopiWisdom"
+  },
+  {
+    text: "He who is present at a wrongdoing and does not lift a hand to prevent it is as guilty as the wrongdoers.",
+    author: "Interpretation Emphasizes the responsibility to take action and prevent harm when witnessing wrongdoing. #HopiWisdom"
+  },
+  {
+    text: "A kind word warms for a few seconds; a kind deed warms for a lifetime.",
+    author: "Interpretation Highlights the lasting impact of acts of kindness compared to words alone. #HopiWisdom"
+  },
+  {
+    text: "In a world where you can be anything, be kind.",
+    author: "Interpretation Encourages choosing kindness as a guiding principle in interactions with others. #HopiWisdom"
+  },
+  {
+    text: "The best fighter is never angry.",
+    author: "Interpretation Encourages maintaining composure and a calm mind in the face of challenges. #HopiWisdom"
+  },
+  {
+    text: "Silence is the fence around wisdom.",
+    author: "Interpretation Suggests that sometimes wisdom is best conveyed through silence and contemplation. #HopiWisdom"
+  },
+  {
+    text: "He who fears the sun will not become chief.",
+    author: "Interpretation Encourages facing challenges and embracing opportunities for growth. #HopiWisdom"
+  },
+  {
+    text: "Do not be too timid and squeamish about your actions. All life is an experiment.",
+    author: "Interpretation Encourages embracing life with courage and viewing experiences as opportunities to learn and grow. #HopiWisdom"
+  },
+  {
+    text: "Don't be pushed around by the fears in your mind. Be led by the dreams in your heart.",
+    author: "Interpretation Encourages pursuing one's dreams and aspirations despite internal fears and doubts. #HopiWisdom"
+  },
+  {
+    text: "The strongest people are not those who show strength in front of us but those who win battles we know nothing about.",
+    author: "Interpretation Acknowledges the strength that can be hidden behind the scenes, beyond what is apparent. #HopiWisdom"
+  },
+  {
+    text: "The soul would have no rainbow if the eyes had no tears.",
+    author: "Interpretation Emphasizes the beauty and growth that can come from overcoming challenges and difficulties. #HopiWisdom"
+  },
+  {
+    text: "One finger cannot lift a pebble.",
+    author: "Interpretation Highlights the importance of collaboration and unity in achieving common goals. #HopiWisdom"
+  },
+  {
+    text: "The more you give, the more you receive.",
+    author: "Interpretation Encourages generosity and the understanding that giving can lead to personal fulfillment. #HopiWisdom"
+  },
+  {
+    text: "Nature is not a place to visit. It is home.",
+    author: "Interpretation Encourages a deep connection and respect for nature as an integral part of our existence. #HopiWisdom"
+  },
+  {
+    text: "You are never lost; you are simply on a journey with no end.",
+    author: "Interpretation Encourages viewing life as a continuous journey with opportunities for growth and discovery. #HopiWisdom"
+  },
 ];
