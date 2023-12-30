b5b8d7965e21fa80b97e56f3420b565c07459e45
Josh Langfield <43525627+jlangfield@users.noreply.github.com>
2023-12-30 14:45:58 -0600

    Add files via upload

--- quotes8.js
+++ quotes8.js
@@ -99,4 +99,216 @@ const localQuotes8 = [
         text: "The spider weaves intricate webs; may your endeavors be woven with care and creativity.",
         author: "Interpretation: Symbolizes the spider's web as intricate, encouraging endeavors that are carefully crafted and infused with creativity. #NezPerceWisdom"
     },
+    {
+        text: "The butterfly's dance is a celebration of transformation; may you find joy in your evolving journey.",
+        author: "Interpretation: Symbolizes the butterfly's dance as a celebration of transformation, wishing for joy in the ongoing journey of personal evolution. #NezPerceWisdom"
+    },
+    {
+        text: "The thunderous drum echoes the heartbeat of Mother Earth; may your spirit resonate in harmony.",
+        author: "Interpretation: Uses the drumbeat to symbolize a connection to Mother Earth, wishing for a spirit that resonates in harmony with the heartbeat of the planet. #NezPerceTeaching"
+    },
+    {
+        text: "The meadowlark's song is a melody of joy; may your days be filled with uplifting tunes.",
+        author: "Interpretation: Symbolizes the meadowlark's song as a melody of joy, wishing for days filled with uplifting and positive experiences. #NezPerceProverb"
+    },
+    {
+        text: "The river's flow mirrors the passage of time; may your journey be embraced with acceptance and adaptability.",
+        author: "Interpretation: Uses the river's flow to symbolize life's cycles, encouraging acceptance and adaptability in one's journey. #NezPerceWisdom"
+    },
+    {
+        text: "The moon's reflection on water mirrors the cyclical nature of life; may you find peace in its phases.",
+        author: "Interpretation: Uses the moon's reflection to symbolize life's cycles, encouraging finding peace and acceptance in the phases of existence. #NezPerceTeaching"
+    },
+    {
+        text: "The huckleberry is a gift from the earth; may your days be sweetened with the fruits of gratitude.",
+        author: "Interpretation: Symbolizes huckleberries as gifts from the earth, encouraging a life sweetened by the practice of gratitude. #NezPerceProverb"
+    },
+    {
+        text: "The eagle's flight is a dance with the winds; may your spirit dance freely in the currents of life.",
+        author: "Interpretation: Uses the eagle's flight as a metaphor for spiritual freedom, wishing for a spirit that dances freely in the currents of life. #NezPerceWisdom"
+    },
+    {
+        text: "The salmon's journey upstream is a testament to perseverance; may you overcome obstacles with determination.",
+        author: "Interpretation: Symbolizes the salmon's journey as perseverance, encouraging the overcoming of obstacles with steadfast determination. #NezPerceTeaching"
+    },
+    {
+        text: "The cedar tree offers protection and wisdom; may your actions provide shelter and insight to others.",
+        author: "Interpretation: Symbolizes the cedar tree's attributes, urging actions that provide both shelter and wisdom to those in need. #NezPerceProverb"
+    },
+    {
+        text: "The hummingbird's flight is a dance of joy; may your life echo with the same exuberance.",
+        author: "Interpretation: Symbolizes the hummingbird's flight as a dance of joy, wishing for a life filled with exuberance and happiness. #NezPerceWisdom"
+    },
+    {
+        text: "The wolf teaches the importance of family; may your pack be a source of strength and unity.",
+        author: "Interpretation: Draws inspiration from the wolf's social structure, wishing for strong and unified familial bonds. #NezPerceTeaching"
+    },
+    {
+        text: "The meandering river teaches patience; may you flow with the currents of time.",
+        author: "Interpretation: Uses the meandering river as a symbol of patience, encouraging a fluid and patient approach to the passage of time. #NezPerceProverb"
+    },
+    {
+        text: "The morning dew is a reminder of the earth's tears; may you tread lightly and with compassion.",
+        author: "Interpretation: Symbolizes morning dew as the earth's tears, urging gentle and compassionate actions in one's journey. #NezPerceWisdom"
+    },
+    {
+        text: "The songs of the ancestors linger in the wind; may you carry their melodies in your heart.",
+        author: "Interpretation: Symbolizes ancestral songs in the wind, urging the remembrance and embodiment of the wisdom passed down through generations. #NezPerceTeaching"
+    },
+    {
+        text: "The buffalo's wisdom is written in the patterns of its fur; may you decipher life's messages with clarity.",
+        author: "Interpretation: Draws wisdom from the buffalo's patterns, wishing for clarity in understanding and interpreting life's messages. #NezPerceProverb"
+    },
+    {
+        text: "The cedar tree stands tall in the forest; may your stature be one of strength and dignity.",
+        author: "Interpretation: Symbolizes the cedar tree's stature, encouraging personal strength and dignity in navigating life's challenges. #NezPerceWisdom"
+    },
+    {
+        text: "The coyote's howl is a call to adaptability; may you navigate change with resilience and flexibility.",
+        author: "Interpretation: Symbolizes the coyote's howl as a call to adaptability, urging resilience and flexibility in the face of change. #NezPerceTeaching"
+    },
+    {
+        text: "The spider weaves intricate webs; may your endeavors be woven with care and creativity.",
+        author: "Interpretation: Symbolizes the spider's web as intricate, encouraging endeavors that are carefully crafted and infused with creativity. #NezPerceProverb"
+    },
+    {
+        text: "The laughter of the river is a melody of joy; may your life be a harmonious symphony.",
+        author: "Interpretation: Symbolizes the river's laughter as a melody of joy, wishing for a life that resonates as a harmonious and joyous symphony. #NezPerceWisdom"
+    },
+    {
+        text: "The fire of passion ignites the soul; may your pursuits be fueled by a burning, purposeful flame.",
+        author: "Interpretation: Uses the fire of passion to symbolize soulful ignition, encouraging pursuits driven by a purposeful and impactful flame. #NezPerceTeaching"
+    },
+    {
+        text: "The shadows of the forest reveal hidden wonders; may your journey unveil unexpected treasures.",
+        author: "Interpretation: Uses the shadows of the forest as a metaphor, wishing for a journey that reveals hidden wonders and unexpected treasures. #NezPerceProverb"
+    },
+    {
+        text: "The snowflake's intricate design is a reminder of the uniqueness within each individual; celebrate diversity.",
+        author: "Interpretation: Symbolizes the snowflake's design, urging the celebration of individual uniqueness and the beauty found in diversity. #NezPerceWisdom"
+    },
+    {
+        text: "The drumbeat is the heartbeat of the community; may you contribute to its rhythm with love and respect.",
+        author: "Interpretation: Uses the drumbeat as a symbol of community heartbeat, encouraging contributions with love and respect to the collective rhythm. #NezPerceTeaching"
+    },
+    {
+        text: "The rainbow's colors blend in harmony; may your relationships reflect the beauty of unity.",
+        author: "Interpretation: Uses the rainbow's colors as symbols of unity, wishing for harmonious and beautiful relationships. #NezPerceProverb"
+    },
+    {
+        text: "The teachings of the elders are a treasure; may you cherish and pass them on to the next generation.",
+        author: "Interpretation: Likens elder teachings to treasures, urging their appreciation and transmission for the benefit of future generations. #NezPerceWisdom"
+    },
+    {
+        text: "The thunderstorm cleanses the air and revitalizes the earth; may challenges purify and renew your spirit.",
+        author: "Interpretation: Uses the metaphor of a thunderstorm to represent challenges that cleanse and renew, fostering spiritual growth and resilience. #NezPerceTeaching"
+    },
+    {
+        text: "The spider weaves intricate webs; may your endeavors be woven with care and creativity.",
+        author: "Interpretation: Symbolizes the spider's web as intricate, encouraging endeavors that are carefully crafted and infused with creativity. #NezPerceProverb"
+    },
+    {
+        text: "The eagle's vision sees beyond the horizon; may your perspective encompass vast possibilities.",
+        author: "Interpretation: Uses the eagle's vision as a metaphor for broad perspective, encouraging an outlook that sees vast possibilities. #NezPerceWisdom"
+    },
+    {
+        text: "The bear's hibernation is a time of introspection; may you find wisdom in moments of quiet reflection.",
+        author: "Interpretation: Symbolizes the bear's hibernation as introspective, wishing for wisdom gained through quiet moments of self-reflection. #NezPerceTeaching"
+    },
+    {
+        text: "The grass beneath your feet tells stories of the land's history; may you walk with reverence and understanding.",
+        author: "Interpretation: Encourages walking with reverence, acknowledging the stories embedded in the land and seeking understanding of its history. #NezPerceProverb"
+    },
+    {
+        text: "The coyote's cleverness is a guide to navigating challenges; may you find solutions with wit and resourcefulness.",
+        author: "Interpretation: Draws inspiration from the coyote's cleverness, encouraging the use of wit and resourcefulness in navigating life's challenges. #NezPerceWisdom"
+    },
+    {
+        text: "The rainbow's arch spans the sky; may your endeavors reach great heights of accomplishment.",
+        author: "Interpretation: Uses the rainbow's arch as a symbol of accomplishment, wishing for success and achievement in one's endeavors. #NezPerceTeaching"
+    },
+    {
+        text: "The owl's wisdom is a gift from the night; may you receive insights in moments of darkness.",
+        author: "Interpretation: Symbolizes the owl's wisdom as insights gained in darkness, encouraging the appreciation of lessons learned in challenging times. #NezPerceProverb"
+    },
+    {
+        text: "The buffalo's roaming is a dance with the prairie; may your journey be a dance with the landscape of life.",
+        author: "Interpretation: Draws inspiration from the buffalo's roaming, encouraging a life journey that dances in harmony with the landscapes encountered. #NezPerceWisdom"
+    },
+    {
+        text: "The river's flow is a reminder of constant change; may you embrace transformation with open arms.",
+        author: "Interpretation: Uses the river's flow to symbolize change, encouraging an open-hearted embrace of transformation and growth. #NezPerceTeaching"
+    },
+    {
+        text: "The drumbeat is a call to unity; may your actions contribute to the harmonious rhythm of community.",
+        author: "Interpretation: Uses the drumbeat as a call to unity, urging actions that contribute to the harmonious rhythm of community life. #NezPerceProverb"
+    },
+    {
+        text: "The spider weaves its web with purpose; may your goals be pursued with intention and diligence.",
+        author: "Interpretation: Symbolizes the spider's web as purposeful, encouraging the pursuit of goals with intention and diligence. #NezPerceWisdom"
+    },
+    {
+        text: "The sun's warmth is a gift to the earth; may your presence be a source of warmth and kindness.",
+        author: "Interpretation: Symbolizes the sun's warmth as a gift, wishing for one's presence to radiate warmth and kindness to others. #NezPerceTeaching"
+    },
+    {
+        text: "The mountain's summit is a testament to perseverance; may you overcome challenges with steadfast determination.",
+        author: "Interpretation: Uses the mountain's summit as a symbol of perseverance, encouraging the overcoming of challenges with determination. #NezPerceProverb"
+    },
+    {
+        text: "The salmon's leap is a dance of determination; may you navigate life's currents with resilience.",
+        author: "Interpretation: Symbolizes the salmon's leap as a dance of determination, wishing for resilience in navigating the currents of life. #NezPerceWisdom"
+    },
+    {
+        text: "The horse's spirit is wild and free; may your essence be untamed and unrestricted.",
+        author: "Interpretation: Draws inspiration from the horse's spirit, encouraging a wild and free essence that embraces life with unrestricted enthusiasm. #NezPerceTeaching"
+    },
+    {
+        text: "The moon's phases mark the passage of time; may your days be filled with purpose and intention.",
+        author: "Interpretation: Uses the moon's phases to symbolize the passage of time, urging a life filled with purposeful and intentional actions. #NezPerceProverb"
+    },
+    {
+        text: "The butterfly's metamorphosis is a dance of rebirth; may you embrace change with grace.",
+        author: "Interpretation: Symbolizes the butterfly's metamorphosis as a graceful dance of rebirth, urging the embrace of change with elegance. #NezPerceWisdom"
+    },
+    {
+        text: "The deer's agility is a lesson in adaptability; may you navigate life's challenges with grace.",
+        author: "Interpretation: Draws inspiration from the deer's agility, encouraging adaptability and graceful navigation of life's challenges. #NezPerceTeaching"
+    },
+    {
+        text: "The drumbeat is the heartbeat of the community; may you contribute to its rhythm with love and respect.",
+        author: "Interpretation: Uses the drumbeat as a symbol of community heartbeat, encouraging contributions with love and respect to the collective rhythm. #NezPerceProverb"
+    },
+    {
+        text: "The rainbow's colors blend in harmony; may your relationships reflect the beauty of unity.",
+        author: "Interpretation: Uses the rainbow's colors as symbols of unity, wishing for harmonious and beautiful relationships. #NezPerceWisdom"
+    },
+    {
+        text: "The teachings of the elders are a treasure; may you cherish and pass them on to the next generation.",
+        author: "Interpretation: Likens elder teachings to treasures, urging their appreciation and transmission for the benefit of future generations. #NezPerceTeaching"
+    },
+    {
+        text: "The thunderstorm cleanses the air and revitalizes the earth; may challenges purify and renew your spirit.",
+        author: "Interpretation: Uses the metaphor of a thunderstorm to represent challenges that cleanse and renew, fostering spiritual growth and resilience. #NezPerceProverb"
+    },
+    {
+        text: "The spider weaves intricate webs; may your endeavors be woven with care and creativity.",
+        author: "Interpretation: Symbolizes the spider's web as intricate, encouraging endeavors that are carefully crafted and infused with creativity. #NezPerceWisdom"
+    },
+    {
+        text: "The laughter of the river is a melody of joy; may your life be a harmonious symphony.",
+        author: "Interpretation: Symbolizes the river's laughter as a melody of joy, wishing for a life that resonates as a harmonious and joyous symphony. #NezPerceTeaching"
+    },
+    {
+        text: "The fire of passion ignites the soul; may your pursuits be fueled by a burning, purposeful flame.",
+        author: "Interpretation: Uses the fire of passion to symbolize soulful ignition, encouraging pursuits driven by a purposeful and impactful flame. #NezPerceProverb"
+    },
+    {
+        text: "The shadows of the forest reveal hidden wonders; may your journey unveil unexpected treasures.",
+        author: "Interpretation: Uses the shadows of the forest as a metaphor, wishing for a journey that reveals hidden wonders and unexpected treasures. #NezPerceWisdom"
+    },
+    {
+        text: "The snowflake's intricate design is a reminder of the uniqueness within each individual; celebrate diversity.",
+        author: "Interpretation: Symbolizes the snowflake's design, urging the celebration of individual uniqueness and the beauty found in diversity. #NezPerceSaying"
+    },
 ];
